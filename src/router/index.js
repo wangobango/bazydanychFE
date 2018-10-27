88940474cc4e4049fadb5e10e2c4b34dfbf287aa
wangobango <puch003@wp.pl>
2018-10-27 16:17:06 +0200

    temp login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import LogInPage from '@/components/layout/LogInPage.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+  mode: "history",
+  routes: [{
+    path: '/',
+    name: 'LogIn',
+    component: LogInPage
+  }],
+  scrollBehavior(to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0
     }
-  ]
+  }
 })
