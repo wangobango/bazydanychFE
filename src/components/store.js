fa279cf411dbc31089f271a9b801db6f02c114eb
wangobango <puch003@wp.pl>
2018-11-16 16:49:36 +0100

    added header

--- src/components/store.js
+++ src/components/store.js
@@ -8,7 +8,7 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state:{
         user:{
-            authorizedUser:'False',
+            authorizedUser:'True',
             token: "",   
         },
     },
